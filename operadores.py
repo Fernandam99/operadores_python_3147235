683e7bead24a5a57504a37ca7a25b7ae96ceb1fd
--replace-all <--replace-all>
2025-02-28 09:43:09 -0500

    parentesis

--- operadores.py
+++ operadores.py
@@ -1,15 +1,18 @@
 # operadores en python
 ''' los operadores en python siguen el siguiente orden jeraquico:
-1. ** (exponente)
-2. / , * , %,
-3. + , -
-4. =
-si hay operaciones en el ismo nivel de jerarquia se resuelven de izquierda a derecha
+1. ()
+2. ** exponente
+3. / , * , %,
+4. + , -
+5. =
+nota 1: si hay operaciones en el mismo nivel de jerarquia se resuelven de izquierda a derecha
+nota 2: si hay parentesis detro del paresis se resuelve primero el parentesis interno
 '''
 
 a = 3
 b = 2
 c = 1
+x = 5
 
-y = a ** 2 - a * b  - 1 / 3 + c
+y = ((2*a+c)/7)*(a+(9*a)/c)
 print(y)
