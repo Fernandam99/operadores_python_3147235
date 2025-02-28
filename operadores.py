b5196c71ed07f69bb311c9b67b44ff4df7441800
--replace-all <--replace-all>
2025-02-28 09:06:07 -0500

    operaciones con la misma jerarquia

--- operadores.py
+++ operadores.py
@@ -1,13 +1,15 @@
 # operadores en python
 ''' los operadores en python siguen el siguiente orden jeraquico:
-1. / , * , %,
-2. + , -
-3. =
+1. ** (exponente)
+2. / , * , %,
+3. + , -
+4. =
+si hay operaciones en el ismo nivel de jerarquia se resuelven de izquierda a derecha
 '''
 
 a = 3
 b = 2
 c = 1
 
-y = a * b + c
+y = a ** 2 - a * b  - 1 / 3 + c
 print(y)
