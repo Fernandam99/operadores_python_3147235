d185f3adbc19894b196c7b78413620bc752115d5
--replace-all <stev508ga@gmail.com>
2025-03-07 08:58:40 -0500

    operadores buleanos o logicos

--- operadores_logicos.py
+++ operadores_logicos.py
@@ -5,7 +5,24 @@ obedecen las ttablas de verdad:
 
 '''
 
-op1 = false
-op2 = true
-op3 = op1 and op2
+op1 = False
+op2 = True
+op3 = op1 or op2
 print(op3)
+
+#Operador not
+op4 = not op2
+print (op4)
+
+''' 
+GERANQUIA DEFINITIVA DE OPERDORES
+1       ()
+2       **
+3      +,/,%
+4       +,-
+5 >,<,!=,==,<=,>=
+6       not
+7       and
+8        or
+9        =
+'''
