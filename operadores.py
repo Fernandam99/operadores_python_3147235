d9baca30334a0c4a2387c2aa9bee2d87ad5a919b
--replace-all <stev508ga@gmail.com>
2025-03-07 07:05:15 -0500

    operadores

--- operadores.py
+++ operadores.py
@@ -7,12 +7,29 @@
 5. =
 nota 1: si hay operaciones en el mismo nivel de jerarquia se resuelven de izquierda a derecha
 nota 2: si hay parentesis detro del paresis se resuelve primero el parentesis interno
+
 '''
 
-a = 3
-b = 2
-c = 1
+a = 2
+b = 3
+c = 7
 x = 5
+h = 1
 
-y = ((2*a+c)/7)*(a+(9*a)/c)
+y = c / (x + 2)< c*a-c+1-b*2
 print(y)
+
+
+'''
+operadores relacionales:
+las operaciones aritmeticas resultan en un valor numerico
+las operaciones relaciones resultran en un valor booleano: tru false (v, f si, no)
+operadores relacionales: > , < , >= , <= ,  != , ==
+
+jerarquia de operadores incluyendo los relacionales
+1. ()
+2. **
+3. * , / , % 
+4. + , -
+5. > , < , >= , <= , != ,==
+'''
