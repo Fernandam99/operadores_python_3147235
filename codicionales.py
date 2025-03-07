45098033566807665e3b83a6df57508433d1a980
--replace-all <stev508ga@gmail.com>
2025-03-07 09:49:52 -0500

    condicionales

---
+++ codicionales.py
@@ -0,0 +1,12 @@
+'''
+CONDICIONAL:
+Es una wexpresión que al ser evaluado da coo resultado verdadero o falso por lo tanto es una condicional
+deben haber operadores relacionales o logicos
+'''
+#ejemplo de condicional
+a=3
+b=2
+c=1
+d=4
+resultado=((a+b)*c)**2<4 and (b+d)/2>3
+print(resultado)
